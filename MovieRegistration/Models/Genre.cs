01e7d78f851e0573ca7691febad32ab23a745d5e
m4stersplint3r <m4stersplint3r@gmail.com>
2020-11-16 23:33:55 -0500

    first commit

---
+++ MovieRegistration/Models/Genre.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Models
+{
+    public enum Genre
+    {
+        Action,
+        Comedy,
+        Drama,
+        Horror,
+        Romance,
+        Western,
+        Thriller,
+        Adventure,
+        Documentary,
+        Musical,
+        Mystery
+    }
+}
---
+++ MovieRegistration/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "You must enter a Title")]
+        [StringLength(50, ErrorMessage = "The Title is too long.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "You must select a Genre")]
+        public Genre MovieGenre { get; set; }
+        [Range(1880, 2020, ErrorMessage = "The year chosen is impossible. Try again.")]
+        [Required(ErrorMessage = "You must enter a Year")]
+        public int Year { get; set; }
+        public string Actors { get; set; }
+        public string Directors { get; set; }
+    }
+}
